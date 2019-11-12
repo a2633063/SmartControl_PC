65761aca4d6106c69ae4ebb9f05302004a66bebd
Zip <76966589@qq.com>
2019-11-12 18:28:43 +0800

    add:增加公司名称

--- ZControl/Properties/AssemblyInfo.cs
+++ ZControl/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ZControl")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Zip")]
 [assembly: AssemblyProduct("ZControl")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
