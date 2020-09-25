6542ad5591d0f2e4a173615edeb248f67789503d
Zip <76966589@qq.com>
2020-09-25 10:38:37 +0800

    fix: 窗口标题错误

--- ZControl/Form1.Designer.cs
+++ ZControl/Form1.Designer.cs
@@ -419,7 +419,7 @@
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "ZContron PC端";
+            this.Text = "ZControl PC端";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.groupBoxMQTT.ResumeLayout(false);
