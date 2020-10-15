6d5fb26b557c27cb503a49623b4acd60ceb7bed1
Zip <76966589@qq.com>
2020-10-15 08:51:48 +0800

    add: 补充缺少的文件

---
+++ ZControl/FormDialogDeviceImport.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ZControl
+{
+    partial class FormDialogDeviceImport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDeviceJson = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "在这里粘贴:";
+            // 
+            // txtDeviceJson
+            // 
+            this.txtDeviceJson.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDeviceJson.Location = new System.Drawing.Point(12, 24);
+            this.txtDeviceJson.Multiline = true;
+            this.txtDeviceJson.Name = "txtDeviceJson";
+            this.txtDeviceJson.Size = new System.Drawing.Size(389, 397);
+            this.txtDeviceJson.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(12, 427);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(389, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormDialogDeviceImport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 476);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDeviceJson);
+            this.Controls.Add(this.label1);
+            this.Name = "FormDialogDeviceImport";
+            this.Text = "导入设备";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDeviceJson;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ ZControl/FormDialogDeviceImport.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZControl
+{
+    public partial class FormDialogDeviceImport : Form
+    {
+        String returnString=null;
+        public FormDialogDeviceImport()
+        {
+            InitializeComponent();
+        }
+        public new string ShowDialog()
+        {
+            var result = base.ShowDialog();
+
+            return returnString;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = txtDeviceJson.Text;
+            try
+            {
+                JObject jObject = JObject.Parse(s);
+                returnString = s;
+                this.Close();
+            }
+            catch (Exception)
+            {
+                returnString = null;
+                MessageBox.Show("输入内容不是Json格式!", "错误",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //throw;
+            }
+        }
+    }
+}
